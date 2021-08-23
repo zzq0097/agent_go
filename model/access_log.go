6f946e87ab6d22c5192a31e8e2774c15232458c7
ZZQ <18736271057@163.com>
2021-08-23 22:14:04 +0800

    -

---
+++ model/access_log.go
@@ -0,0 +1,20 @@
+package model
+
+// AccessLog
+// Nginx log_format 参数
+// log_format json '{"remoteUser":"$remote_user","request":"$request","upstreamResponseTime":"$upstream_response_time","requestLength":"$request_length","httpUserAgent":"$http_user_agent","requestTime":"$request_time","httpHost":"$http_host","httpReferer":"$http_referer","remoteAddr":"$remote_addr","timeLocal":"$time_local","status":"$status","upstreamStatus":"$upstream_status","bodyBytesDent":"$body_bytes_sent"}'
+type AccessLog struct {
+	RemoteUser           string `json:"remoteUser"`
+	Request              string `json:"request"`
+	UpstreamResponseTime string `json:"upstreamResponseTime"`
+	RequestLength        string `json:"requestLength"`
+	HttpUserAgent        string `json:"httpUserAgent"`
+	RequestTime          string `json:"requestTime"`
+	HttpHost             string `json:"httpHost"`
+	HttpReferer          string `json:"httpReferer"`
+	RemoteAddr           string `json:"remoteAddr"`
+	TimeLocal            string `json:"timeLocal"`
+	Status               string `json:"status"`
+	BodyBytesDent        string `json:"bodyBytesDent"`
+	UpstreamStatus       string `json:"upstreamStatus"`
+}
